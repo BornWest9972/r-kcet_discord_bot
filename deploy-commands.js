6e6b3737bdd45415d7c0f5fe929e602e9bf15263
farhan291 <farhan29110511@gmail.com>
2025-06-02 23:08:24 +0530

    added new guild id

--- deploy-commands.js
+++ deploy-commands.js
@@ -6,13 +6,7 @@ require('dotenv').config();
 
 const token = process.env.Token;
 const clientId = process.env.clientId;
-const guildId = process.env.guildId;
-
-
-
-
-
-
+const guildIds = process.env.guildIds.split(','); // multiple guilds
 
 // Loading commands 
 const commands = [];
@@ -21,22 +15,25 @@ const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('
 
 for (const file of commandFiles) {
   const command = require(`./commands/${file}`);
-  commands.push(command.data.toJSON()); // Slash commands covert to JSON for discord rest api
+  commands.push(command.data.toJSON());
 }
 
-// Deploy them to the guild
+// Deploy to each guild
 const rest = new REST({ version: '10' }).setToken(token);
 
 (async () => {
   try {
     console.log('Started refreshing application (/) commands.');
 
-    await rest.put(
-      Routes.applicationGuildCommands(clientId, guildId),
-      { body: commands }
-    );
+    for (const guildId of guildIds) {
+      await rest.put(
+        Routes.applicationGuildCommands(clientId, guildId.trim()),
+        { body: commands }
+      );
+      console.log(`Successfully reloaded commands for guild ${guildId.trim()}`);
+    }
 
-    console.log('Successfully reloaded application (/) commands.');
+    console.log('All guilds updated.');
   } catch (error) {
     console.error(error);
   }
